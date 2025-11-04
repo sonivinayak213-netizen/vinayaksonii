e3b54946cd06141e401e7036a98099adef9af1b4
sonivinayak213-netizen <sonivinayak213@gmail.com>
2025-11-04 12:45:15 +0530

    Update page.js
    added homepage file

--- app/page.js
+++ app/page.js
@@ -1,1 +1,15 @@
+'use client';
+
+export default function Home() {
+  return (
+    <div style={{
+      textAlign: 'center',
+      marginTop: '100px',
+      fontFamily: 'sans-serif'
+    }}>
+      <h1>Welcome to Vinayak Trader Pro 🚀</h1>
+      <p>Your deployment is live and working fine!</p>
+    </div>
+  );
+}
 
